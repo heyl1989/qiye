e2fe862c4860ce6c851243b691c4313788ffe355
Yongliang He <heyl2804@gmail.com>
2019-05-28 18:06:00 +0800

    系统设置

--- application/admin/controller/System.php
+++ application/admin/controller/System.php
@@ -20,68 +20,18 @@ class System extends Base
     }
 
     /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-        //
-    }
-
-    /**
-     * 显示指定的资源
-     *
-     * @param  int $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
-
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * 保存更新的资源
      *
      * @param  \think\Request $request
      * @param  int $id
      * @return \think\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        //1判断提交类型
+        if ($request->isAjax(true)){
+
+        }
     }
 
-    /**
-     * 删除指定资源
-     *
-     * @param  int $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
-    }
 }
