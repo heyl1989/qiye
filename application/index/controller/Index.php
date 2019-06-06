98b65839b5861138126d4d4e6812ac2f7ff09e56
Yongliang He <heyl2804@gmail.com>
2019-06-06 15:49:31 +0800

    前台banner

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,11 +2,17 @@
 namespace app\index\controller;
 
 use app\admin\common\Base;
+use app\admin\model\Banner;
 
 class Index extends Base
 {
     public function index()
     {
+        //1.获取banner数据
+        $banner = Banner::all();
+        //2.模板赋值
+        $this->view->assign('banner',$banner);
+        //3.模板渲染
         return $this->view->fetch('index');
     }
 }
