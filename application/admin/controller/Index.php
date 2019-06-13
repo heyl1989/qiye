84ce2124c8344371b1aab77a5ccc3ec3b0033ec5
Yongliang He <heyl2804@gmail.com>
2019-06-13 15:52:45 +0800

    app接口

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace app\admin\controller;
 
 use app\admin\common\Base;
+use think\Request;
 
 class Index extends Base
 {
@@ -15,4 +17,6 @@ class Index extends Base
     {
         return $this->view->fetch('welcome');
     }
+
+
 }
--- application/route.php
+++ application/route.php
@@ -8,14 +8,19 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+use think\Route;
+//Route::rule('testApi', 'app/Index/getTestData');
 
 return [
+
     '__pattern__' => [
         'name' => '\w+',
     ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+    '[hello]' => [
+        ':id' => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
     ],
 
 ];
+
+
