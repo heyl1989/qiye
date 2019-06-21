172b4552ce6dd73f72dac008513524e11ca79503
Administrator <heyongliang>
2019-06-21 20:40:59 +0800

    weixin

--- application/wx/controller/Index.php
+++ application/wx/controller/Index.php
@@ -63,8 +63,8 @@ class Index
     private function responseMsg()
     {
         //get post data, May be due to the different environments
-        $postStr = isset($GLOBALS["HTTP_RAW_POST_DATA"]) ? $GLOBALS["HTTP_RAW_POST_DATA"] : '';
-
+//        $postStr = isset($GLOBALS["HTTP_RAW_POST_DATA"]) ? $GLOBALS["HTTP_RAW_POST_DATA"] : '';
+        $postStr = file_get_contents("php://input");
         //extract post data
         if (!empty($postStr)) {
             /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
