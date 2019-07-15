88ad61aae0df3bcb00c1453075a1f6392b87f171
Yongliang He <heyl2804@gmail.com>
2019-07-15 16:22:50 +0800

    前台代码修改

--- application/wx/controller/Index.php
+++ application/wx/controller/Index.php
@@ -105,6 +105,26 @@ class Index
                 echo $resultStr;
             }
             if (!empty($keyword)) {
+                if(strpos($keyword, "商城") !== false){
+                    $textTpl = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[news]]></MsgType>
+					<ArticleCount>1</ArticleCount>
+					<Articles>
+					<item>
+					<Title><![CDATA[欢迎来到甘肃永登]]></Title> 
+					<Description><![CDATA[永登，古称令居、庄浪，隶属于甘肃省兰州市]]></Description>
+					<PicUrl><![CDATA[http://img1.imgtn.bdimg.com/it/u=2572839016,2682225406&fm=214&gp=0.jpg]]></PicUrl>
+					<Url><![CDATA[http://47.94.234.255/qiye/public/]]></Url>
+					</item>
+					</Articles>
+					</xml>";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time);
+                    echo $resultStr;
+                    return;
+                }
                 $msgType = "text";
                 $contentStr = '您输入了：' . $keyword;
                 if (strpos($keyword, "天气") !== false) {
