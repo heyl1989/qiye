2123135fb27139e7a6e176240c4aeb17d68d81c5
Yongliang He <heyl2804@gmail.com>
2019-07-15 15:57:16 +0800

    前台代码修改

--- application/wx/controller/Menu.php
+++ application/wx/controller/Menu.php
@@ -16,48 +16,11 @@ class Menu
          "button":[
          {    
               "type":"click",
-              "name":"今日歌曲",
-              "key":"V1001_TODAY_MUSIC"
-          },
-          {
-               "name":"菜单",
-               "sub_button":[
-               {    
-                   "type":"view",
-                   "name":"搜索",
-                   "url":"http://www.soso.com/"
-                },
-                {
-                     "type":"miniprogram",
-                     "name":"wxa",
-                     "url":"http://mp.weixin.qq.com",
-                     "appid":"wx286b93c14bbf93aa",
-                     "pagepath":"pages/lunar/index"
-                 },
-                {
-                   "type":"click",
-                   "name":"赞一下我们",
-                   "key":"V1001_GOOD"
-                }]
-           }]
+              "name":"天气预报",
+          }]
     }';
 
-    public function index()
-    {
-        //测试获取access_token
-//        $appid = 'wx94655f69745f182f';
-//        $AppSecret = 'da8d44a257a3df74d6795c462cf17ab2';
-//        $file_contents = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$AppSecret");
-//        if (!empty($file_contents)) {
-//            $token = json_decode($file_contents);
-//            $access_token = $token->access_token;
-//            echo $access_token;
-//        }
-        $this->cacheToken();
-    }
-
-
-    public function createMenu($access_token)
+    public function createMenu()
     {
 
     }
@@ -88,7 +51,7 @@ class Menu
             }
         }
         echo $token->access_token;
-//        return $token->access_token;
+        return $token->access_token;
     }
 
     private function getToken()
